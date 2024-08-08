af45abb43214799d3e19394af1046beb74cab7a0
Deepak Vats <deepakvats123123@gmail.com>
2024-08-08 14:25:18 +0530

    register user bugs fixed

--- src/app.js
+++ src/app.js
@@ -5,10 +5,7 @@ import cookieParser from "cookie-parser"
 
 const app = express();
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
-}))
+app.use(cors())
 
 app.use(express.json({limit: "32kb"}));
 app.use(express.urlencoded({extended : true, limit : "32kb"}))
